498368657151bc15f7e10b30583fcb6bac785970
André <andre@dev-next.com>
2020-07-21 16:54:11 -0300

    :tada: First commit

---
+++ packages/api/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: './packages/api/',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      alias: {
+        map: [['~', './src']],
+        extensions: ['.ts', '.js', '.jsx', '.json'],
+      },
+    },
+  },
+  rules: {
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
