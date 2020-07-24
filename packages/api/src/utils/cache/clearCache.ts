1a7a4cfaafa653635a9ba7ad6798a6e5994a2f96
André <andre@dev-next.com>
2020-07-24 18:34:56 -0300

    :bug: api: Fix export

--- packages/api/src/utils/cache/clearCache.ts
+++ packages/api/src/utils/cache/clearCache.ts
@@ -4,8 +4,6 @@ import { promisify } from 'util';
 const client = redis.createClient('redis://127.0.0.1:6379');
 client.hgetPromise = promisify(client.hget);
 
-export default {
-  clearHash(hash = 'tmp'): void {
-    client.del(JSON.stringify(hash));
-  },
-};
+export default function clearHash(hash = 'tmp'): void {
+  client.del(JSON.stringify(hash));
+}
