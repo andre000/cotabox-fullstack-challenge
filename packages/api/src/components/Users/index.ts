6ee92119ffdd199ed46dbdc32aa401b3806a6239
André <andre@dev-next.com>
2020-07-24 18:36:07 -0300

    :sparkles: api: Add User component index

---
+++ packages/api/src/components/Users/index.ts
@@ -0,0 +1,4 @@
+export { default as User } from './UserModel';
+export { default as UserSchema } from './UserSchema';
+export { default as UserController } from './UserController';
+export { IUser } from './UserTypes';
