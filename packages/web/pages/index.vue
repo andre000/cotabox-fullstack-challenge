c9d1ccce45e85436966956229c2c53136d397c1a
André <andre@dev-next.com>
2020-07-29 00:25:58 -0300

    :sparkles: web: Add fetchUser action on index

--- packages/web/pages/index.vue
+++ packages/web/pages/index.vue
@@ -2,13 +2,37 @@
   <div class="container">
     <h1>DATA</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+    <div>
+      <el-row>
+        <el-col :lg="12" :md="24">
+          <main-table :list-users="users" />
+        </el-col>
+        <el-col :lg="12" :md="24">
+          <main-chart :list-users="users" />
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState, mapActions } from 'vuex'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {
+    ...mapState(['users'])
+  },
+
+  created () {
+    this.fetchUsers()
+  },
+
+  methods: {
+    ...mapActions(['fetchUsers'])
+  }
+})
 </script>
 
 <style lang="scss" scoped>
