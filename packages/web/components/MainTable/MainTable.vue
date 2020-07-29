d8e7bbc7edb85800071b5656ba4d5688ca34f961
André <andre@dev-next.com>
2020-07-29 00:20:17 -0300

    :sparkles: web: Create table component

---
+++ packages/web/components/MainTable/MainTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-table :data="listUsers">
+    <el-table-column
+      prop="firstName"
+      label="First Name"
+    />
+    <el-table-column
+      prop="lastName"
+      label="Last Name"
+    />
+    <el-table-column
+      prop="participation"
+      label="Participation"
+    />
+  </el-table>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    listUsers: {
+      type: Array,
+      default: null
+    }
+  }
+})
+</script>
